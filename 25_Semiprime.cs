ea562e6d944d1c3973bcfe11a6bb3f0fc43ef3b0
qgbo <34512893+qgbo@users.noreply.github.com>
2021-06-18 19:48:39 +0800

    Add files via upload

--- 25_Semiprime.cs
+++ 25_Semiprime.cs
@@ -14,11 +14,13 @@ namespace Semiprime
 	}
 	class Solution
 	{
+
+
 		public int[] solution(int N, int[] P, int[] Q)
 		{
 			var primes = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293 };
 
-			var arr= primes.TakeWhile(t => 2 * t <= N).ToArray(); // the mininum value is 2
+			var arr = primes.TakeWhile(t => 2 * t <= N).ToArray(); // the mininum value is 2
 
 			var semiprime = new HashSet<int>();
 
@@ -37,13 +39,75 @@ namespace Semiprime
 			var result = new List<int>();
 			for (int i = 0; i < P.Length; i++)
 			{
-				
+				var s = -1;
+				var b = -1;
+				for (int k = 0; k < semiprimeArr.Count - 1; k++)
+				{
+					if (semiprimeArr[k+1] >= P[i] && s!=-1)
+					{
+						s = k;
+					}
 
-				var s = semiprimeArr.FindIndex(0, semiprimeArr.Count, t => t >= P[i]);
+					if (semiprimeArr[k + 1] >= Q[i] && k != -1)
+					{
+						b = k;
+						continue;
+					}
+				}
 
-				var b = semiprimeArr.FindLastIndex(0, semiprimeArr.Count, t=>t>= Q[i]);
+				if (s == -1)
+				{
+					if (b == -1)
+					{
+						result.Add(0);
+						continue;
+					}
+					else
+						s = 0;
+				}
 
-			
+				result.Add(b - s + 1);
+			}
+
+			return result.ToArray();
+		}
+		public int[] solution4(int N, int[] P, int[] Q)
+		{
+			var primes = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293 };
+
+			var arr = primes.TakeWhile(t => 2 * t <= N).ToArray(); // the mininum value is 2
+
+			var semiprime = new HashSet<int>();
+
+			foreach (var item in arr)
+			{
+				foreach (var s in arr)
+				{
+					semiprime.Add(item * s);
+				}
+			}
+
+			var semiprimeArr = semiprime.Where(t => t <= N).OrderBy(t => t).ToList();
+
+
+
+			var result = new List<int>();
+			for (int i = 0; i < P.Length; i++)
+			{
+				var s = semiprimeArr.FindIndex(t => t >= P[i]);
+
+				var b = semiprimeArr.FindLastIndex(t => t <= Q[i]);
+				if (s == -1)
+				{
+					if (b == -1)
+					{
+						result.Add(0);
+						continue;
+					}
+					else
+						s = 0;
+				}
+
 				result.Add(b - s + 1);
 			}
 
@@ -97,23 +161,23 @@ namespace Semiprime
 
 				var s = semiprimeArr.BinarySearch(P[i]);
 
-				if (b ==s)
+				if (b == s)
 				{
 					result.Add(0);
 					continue;
 				}
 
-				if (b>0)
+				if (b > 0)
 				{
 					if (s >= 0)
-						result.Add(Math.Abs(b -s+1));
+						result.Add(Math.Abs(b - s + 1));
 					else
-						result.Add(Math.Abs(b +2 + s));
+						result.Add(Math.Abs(b + 2 + s));
 				}
-				else 
+				else
 				{
 					if (s >= 0)
-						result.Add(Math.Abs(-b - s - 1) );
+						result.Add(Math.Abs(-b - s - 1));
 					else
 						result.Add(Math.Abs(-b + s));
 				}
@@ -174,38 +238,38 @@ namespace Semiprime
 
 			Random randNum = new Random();
 
-            for (int i = 0; i < 40; i++)
-            {
+			for (int i = 0; i < 40; i++)
+			{
 				continue;
-                int[] test3 = Enumerable.Repeat(0, 24)
-                             .Select(t => randNum.Next(0, 200))
-                             .ToArray();
+				int[] test3 = Enumerable.Repeat(0, 24)
+							 .Select(t => randNum.Next(0, 200))
+							 .ToArray();
 
 
-                int[] test2 = Enumerable.Repeat(0, 24)
-                            .Select(t => randNum.Next(200, 400))
-                            .ToArray();
+				int[] test2 = Enumerable.Repeat(0, 24)
+							.Select(t => randNum.Next(200, 400))
+							.ToArray();
 
-                var s3 = new Solution().solution(30000, test3, test2);
-                var s33 = new Solution().solution2(30000, test3, test2);
-                if (string.Join(",", s3) != string.Join(",", s33))
-                {
-                    Console.WriteLine(string.Join(",", test3));
-                    Console.WriteLine(string.Join(",", test2));
-                    Console.WriteLine();
-                }
-            }
+				var s3 = new Solution().solution(30000, test3, test2);
+				var s33 = new Solution().solution2(30000, test3, test2);
+				if (string.Join(",", s3) != string.Join(",", s33))
+				{
+					Console.WriteLine(string.Join(",", test3));
+					Console.WriteLine(string.Join(",", test2));
+					Console.WriteLine();
+				}
+			}
 
-            //var s = new Solution().solution(20, new int[] { 4, 2, 0, 0, 11, 6, 10, 7, 11, 5 }, new int[] { 0, 4, 11, 6, 7, 3, 8, 2, 11, 8 });
-            //Console.WriteLine(string.Join(",", s));
+			//var s = new Solution().solution(20, new int[] { 4, 2, 0, 0, 11, 6, 10, 7, 11, 5 }, new int[] { 0, 4, 11, 6, 7, 3, 8, 2, 11, 8 });
+			//Console.WriteLine(string.Join(",", s));
 
-            //var ss = new Solution().solution2(20, new int[] { 4, 2, 0, 0, 11, 6, 10, 7, 11, 5 }, new int[] { 0, 4, 11, 6, 7, 3, 8, 2, 11, 8 });
-            //Console.WriteLine(string.Join(",", ss));
+			//var ss = new Solution().solution2(20, new int[] { 4, 2, 0, 0, 11, 6, 10, 7, 11, 5 }, new int[] { 0, 4, 11, 6, 7, 3, 8, 2, 11, 8 });
+			//Console.WriteLine(string.Join(",", ss));
 
-            //         var s1 = new Solution().solution(26, new int[] { 1, 4, 16 }, new int[] { 26, 10, 20 });
-            //Console.WriteLine(string.Join(",", s1));
+			//         var s1 = new Solution().solution(26, new int[] { 1, 4, 16 }, new int[] { 26, 10, 20 });
+			//Console.WriteLine(string.Join(",", s1));
 
-            var  s11 = new Solution().solution(60, new int[] { 10, 4, 7, 19, 6, 5, 4, 16, 8, 17 }, new int[] { 31, 21, 36, 35, 32, 38, 22, 37, 29, 22, 24, 39, 24, 35, 33 });
+			var s11 = new Solution().solution(60, new int[] { 10, 4, 7, 19, 6, 5, 4, 16, 8, 17 }, new int[] { 31, 21, 36, 35, 32, 38, 22, 37, 29, 22, 24, 39, 24, 35, 33 });
 			Console.WriteLine(string.Join(",", s11));
 
 			var s2 = new Solution().solution2(60, new int[] { 10, 4, 7, 19, 6, 5, 4, 16, 8, 17 }, new int[] { 31, 21, 36, 35, 32, 38, 22, 37, 29, 22, 24, 39, 24, 35, 33 });
