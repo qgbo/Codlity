b0a3ebf2a36e555e5c69c36c34e2086e1216d619
qgbo <34512893+qgbo@users.noreply.github.com>
2021-07-01 16:39:24 +0800

    greedy

---
+++ TieRopes.cs
@@ -0,0 +1,31 @@
+﻿
+namespace TieRopes
+{
+    using System;
+    class Solution
+    {
+
+        public int solution(int K, int[] A)
+        {
+            var result = 0;
+            var sum = 0;
+			foreach (var item in A)
+			{
+                sum += item;
+                if (sum >= K)
+                {
+                    sum = 0;
+                    result++;
+                }
+			}
+
+            return result;
+        }
+
+        public static void Test()
+        {
+            var s = new Solution().solution(4,new int[] {1,2,3,4,1,1,3});
+            Console.WriteLine(s);
+        }
+    }
+}
