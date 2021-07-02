b5a4717b7a3cd01cd4308d112ce70841b2990934
qgbo <34512893+qgbo@users.noreply.github.com>
2021-07-02 18:36:39 +0800

    force recuive

--- String/ConvertNnmber.cs
+++ String/ConvertNnmber.cs
@@ -1,44 +1,48 @@
 ﻿
 namespace ConvertNnmber
 {
-    using System;
+	using System;
 
-    class Solution
-    {
-        int num = 0;
-        /// <summary>
-        /// 比如1对应a，2对应b，以此类推，一直到26对应z。 数组字符串有多少种？ 111 有3种，AAA，AK，KA
-        /// </summary>
-        /// <param name="A"></param>
-        /// <returns></returns>
-        public int solution(string str)
-        {
-            if (str.Length == 0)
-                return 0;
+	class Solution
+	{
+		/// <summary>
+		/// 比如1对应a，2对应b，以此类推，一直到26对应z。 数组字符串有多少种？ 111 有3种，AAA，AK，KA
+		/// </summary>
+		/// <param name="A"></param>
+		/// <returns></returns>
+		public int solution(string str)
+		{
+			if (str.Length == 0)
+			{
+				Console.WriteLine(str + ":" + 1);
+				return 1;
+			}
 
-            if (str.Length == 1)
-            {
-                num = 1;
-                return 1;
-            }
+			if (str.Length == 1)
+			{
+				
+				Console.WriteLine(str+":"+ 1);
+				return 1;
+			}
 
 
-            var n = str[0] - '0';
-            var n1 = str[1] - '0';
+			var n = str[0] - '0';
+			var n1 = str[1] - '0';
 
-            if (n * 10 + n1 > 26)
-                num+=  1+solution(str.Substring(2));
-            else
-                num += 1+ solution(str.Substring(1)) +1+ solution(str.Substring(2));
+			var num = solution(str.Substring(2));
 
-            return num;
-        }
+			if (n * 10 + n1 <= 26)
+				num += solution(str.Substring(1));
 
-        public static void Test()
-        {
-           var s= new Solution().solution("12");
+			Console.WriteLine(str + ":" + num);
+			return num;
+		}
 
-            Console.WriteLine(s);
-        }
-    }
+		public static void Test()
+		{
+			var s = new Solution().solution("1283");
+
+			Console.WriteLine(s);
+		}
+	}
 }
