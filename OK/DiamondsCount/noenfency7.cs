5da077ff945612ddc8c223ea2b54308ffabe8428
qgbo <34512893+qgbo@users.noreply.github.com>
2022-09-12 18:43:45 +0800

    Update noenfency7.cs
    correct, better perfomance
    
    1.0.432 sTIMEOUT ERROR, running time: 0.432 sec., time limit: 0.240 sec.
    2.0.428 sTIMEOUT ERROR, running time: 0.428 sec., time limit: 0.240 sec.
    3.0.296 sTIMEOUT ERROR, running time: 0.296 sec., time limit: 0.176 sec.

--- OK/DiamondsCount/noenfency7.cs
+++ OK/DiamondsCount/noenfency7.cs
@@ -1,6 +1,6 @@
 using System;
 // you can also use other imports, for example:
- using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
@@ -16,41 +16,44 @@ class Solution {
 
         int result =0;
 
-     
         // write your code in C# 6.0 with .NET 4.5 (Mono)
 
-         List<Point> points = new List<Point>();
+         bool break2=false;
+
+         Point[] points = new Point[X.Length];
          for (int i = 0; i < X.Length; i++)
             {
-                points.Add(new Point(){X=X[i], Y=Y[i]});
+                points[i]= new Point(){X=X[i],Y=Y[i]};
             }
 
-         points = points.OrderBy(t=>t.X).ThenBy(t=>t.Y).ToList(); 
+         points = points.OrderBy(t=>t.X).ThenBy(t=>t.Y).ToArray(); 
 
-         for (int i = 0; i < points.Count; i++)
+         for (int i = 0; i < points.Length-3; i++)
             {
-
-               for (int j = i+1; j < points.Count; j++)
+               for (int j = i+1; j < points.Length-2; j++)
                 {
+                   
+
                     if(points[i].X==points[j].X)
                            continue;  
 
                     if(points[i].Y > points[j].Y)     
                     {
-                        for (int k = j+1; k < points.Count; k++)
+                        for (int k = j+1; k < points.Length-1; k++)
                         {
-                            if(points[k].X!=points[j].X)
+                            if(points[k].X!=points[j].X) ////////////
+                            {
+                                break2 = true;
                                 break;
+                            }
+                                
 
                             if(points[i].Y >= points[k].Y)
-                               continue;
-
-                            if(points[k].Y + points[j].Y > points[i].Y*2)
-                                break;       
+                                continue;   
 
                             if(points[k].Y + points[j].Y == points[i].Y*2)
                             {
-                                for (int m = k+1; m < points.Count; m++)
+                                for (int m = k+1; m < points.Length; m++)
                                 {
                                     if(points[m].Y == points[i].Y && (points[m].X + points[i].X==points[k].X*2))
                                     {
@@ -58,17 +61,26 @@ class Solution {
                                         break;
                                     }
 
-                                    if(points[m].Y > points[i].Y && (points[m].X > points[i].X))
+                                    if(points[m].Y >= points[i].Y && (points[m].X + points[i].X > points[k].X*2))
                                     {
                                         break;
                                     }
                                 } 
                             }
+
+                             if(points[k].Y + points[j].Y > points[i].Y*2)
+                                break;
+                        }
+
+                        if(break2)
+                        {
+                            break2 = false;
+                            continue;
                         }
                     }
 
-                     if(points[i].Y<=points[j].Y)
-                           break; 
+                    if(points[i].Y<=points[j].Y)
+                            continue; 
                 }
             }
 
