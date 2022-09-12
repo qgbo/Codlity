3089690edc63077d93d8d8bc4be3440f7f0ee8d9
qgbo <34512893+qgbo@users.noreply.github.com>
2022-09-12 14:35:25 +0800

    Update noenfency7.cs
    optimize some conditon

--- OK/DiamondsCount/noenfency7.cs
+++ OK/DiamondsCount/noenfency7.cs
@@ -15,18 +15,13 @@ class Solution {
     public int solution(int[] X, int[] Y) {
 
         int result =0;
+
+        bool ok = false;
         // write your code in C# 6.0 with .NET 4.5 (Mono)
 
          List<Point> points = new List<Point>();
          for (int i = 0; i < X.Length; i++)
             {
-
-                if(!X.Skip(i).Contains(X[i]))
-                break;
-
-                if(!Y.Skip(i).Contains(Y[i]))
-                break;
-
                 points.Add(new Point(){X=X[i], Y=Y[i]});
             }
 
@@ -37,30 +32,47 @@ class Solution {
 
                for (int j = i+1; j < points.Count; j++)
                 {
+                     if(ok)
+                           break;
+                           
                     if(points[i].X==points[j].X)
                            continue;  
 
-                    if(points[i].Y==points[j].Y)
-                           continue;                     
+                    if(points[i].Y<=points[j].Y)
+                           break;                     
                                            
                     for (int k = j+1; k < points.Count; k++)
-                          {
-                             if(points[k].X!=points[j].X)
-                                  continue;   
+                    {
+                        if(ok)
+                           break;
+                        if(points[k].X!=points[j].X)
+                            break;   
 
-                             if(points[i].Y==points[k].Y)
-                                continue; 
+                        if(points[i].Y >= points[k].Y)
+                        continue;
 
-                             if(points[k].Y + points[j].Y != points[i].Y*2)
-                                  continue;  
+                        if(points[k].Y + points[j].Y > points[i].Y*2)
+                            break;       
 
-                             for (int m = k+1; m < points.Count; m++)
-                             {
+                        if(points[k].Y + points[j].Y == points[i].Y*2)
+                        {
+                            for (int m = k+1; m < points.Count; m++)
+                            {
                                 if(points[m].Y == points[i].Y && (points[m].X + points[i].X==points[k].X*2))
+                                {
+                                    ok = false;
                                     result++;
+                                    break;
+                                }
 
-                             } 
-                          }
+                                if(points[m].Y > points[i].Y && (points[m].X > points[i].X))
+                                {
+                                    ok = false;
+                                    break;
+                                }
+                            } 
+                        }
+                    }
                 }
             }
 
